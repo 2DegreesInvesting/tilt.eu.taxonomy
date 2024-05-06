55d7e76ccde116b2635f93f5b0a55f921280db4b
Kalash Singhal <kalash@2degrees-investing.org>
2024-05-06 12:43:46 +0200

    add documentation

--- R/eco_activities_gpt_validation.R
+++ R/eco_activities_gpt_validation.R
@@ -1,8 +1,37 @@
+#' ChatGPT match validation between Ecoinvent activities and EU Taxonomy activities' description
+#'
+#' @param data A dataframe
+#'
+#' @return A dataframe
+#' @export
+#' @examples
+#' \dontrun{
+#' ecoinvent_activities_with_nace <- ecoinvent_activities_with_nace(
+#'   ep_companies_ei_activities |> dplyr::slice(4:6),
+#'   isic_nace_mapper
+#' )
+#'
+#' eu_tax_activities_all <- eu_tax_activities_all(
+#'   eu_tax_climate_mitigation, eu_tax_climate_adaptation,
+#'   eu_tax_water_protection, eu_tax_circular_economy_transition,
+#'   eu_tax_pollution_prevention, eu_tax_biodiversity_protection
+#' )
+#' eu_tax_activities_all_with_keywords <- eu_tax_activities_all_with_keywords(
+#'   eu_tax_activities_all,
+#'   eu_tax_activities_keywords
+#' )
+#'
+#' eco_activities_after_keywords_search <- eco_activities_after_keywords_search(
+#' ecoinvent_activities_eu_tax_activities(
+#'   ecoinvent_activities_with_nace, eu_tax_activities_all_with_keywords
+#' ))
+#'
+#' output <- eco_activities_gpt_validation(eco_activities_after_keywords_search)
+#' output
+#' }
 eco_activities_gpt_validation <- function(data) {
-  gpt_validation <- data |>
+  data |>
     mutate(gpt_validation = unlist(Map(gpt_validate, data$ecoinvent_activity_name, data$tax_activity_description)))
-
-  return(gpt_validation)
 }
 
 gpt_validate <- function(ecoinvent, taxonomy) {
