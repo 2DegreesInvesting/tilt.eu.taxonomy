44c37aaf992fdf5d15f91ab8d010332c08a964cd
Kalash Singhal <kalash@2degrees-investing.org>
2024-05-06 12:00:51 +0200

    refactor

--- R/eco_activities_gpt_validation.R
+++ R/eco_activities_gpt_validation.R
@@ -1,43 +1,6 @@
-#' ChatGPT match validation between Ecoinvent activities and EU Taxonomy activities' description
-#'
-#' @param data A dataframe
-#'
-#' @return A dataframe
-#' @export
-#'
-#' @examples
-#' ecoinvent_activities_with_nace <- ecoinvent_activities_with_nace(
-#'   ep_companies_ei_activities |> dplyr::slice(4:6),
-#'   isic_nace_mapper
-#' )
-#'
-#' eu_tax_activities_all <- eu_tax_activities_all(
-#'   eu_tax_climate_mitigation, eu_tax_climate_adaptation,
-#'   eu_tax_water_protection, eu_tax_circular_economy_transition,
-#'   eu_tax_pollution_prevention, eu_tax_biodiversity_protection
-#' )
-#' eu_tax_activities_all_with_keywords <- eu_tax_activities_all_with_keywords(
-#'   eu_tax_activities_all,
-#'   eu_tax_activities_keywords
-#' )
-#'
-#' eco_activities_after_keywords_search <- eco_activities_after_keywords_search(
-#' ecoinvent_activities_eu_tax_activities(
-#'   ecoinvent_activities_with_nace, eu_tax_activities_all_with_keywords
-#' ))
-#'
-#' output <- eco_activities_gpt_validation(eco_activities_after_keywords_search)
-#' output
 eco_activities_gpt_validation <- function(data) {
-  # file_path <- file.path(getwd(), "inst", "extdata", "access_key.txt")
-  # api_key <- readLines(system.file("access_key.txt", package = "tilt.eu.taxonomy"))
-  # api_key <- readLines('./access_key.txt')
-
   gpt_validation <- data |>
     mutate(gpt_validation = unlist(Map(gpt_validate, data$ecoinvent_activity_name, data$tax_activity_description)))
-
-  # api_key = "---"
-  # rm(api_key)
 
   return(gpt_validation)
 }
@@ -54,20 +17,16 @@ gpt_validate <- function(ecoinvent, taxonomy) {
   'taxonomy_activity_description' column = {%s}
   "
 
-  api_key <- readLines(system.file("access_key.txt", package = "tilt.eu.taxonomy"))
-
   if (is.na(ecoinvent) | is.na(taxonomy)) {
     output <- "No"
   } else {
     prompt_with_data <- sprintf(prompt, ecoinvent, taxonomy)
     output <- create_chat_completion(
       model = "gpt-4-turbo",
-      messages = list(list("role" = "user", "content" = prompt_with_data))
+      messages = list(list("role" = "user", "content" = prompt_with_data)),
+      openai_api_key = openai.api_key
     )$choices$message.content
   }
 
-  api_key = "---"
-  rm(api_key)
-
-  return(output)
+  output
 }
