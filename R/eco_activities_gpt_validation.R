c70d593e7df6059b2792fa2e4df8a8647cb8e060
Kalash Singhal <kalash@2degrees-investing.org>
2024-05-06 11:17:06 +0200

    refactor

--- R/eco_activities_gpt_validation.R
+++ R/eco_activities_gpt_validation.R
@@ -29,16 +29,9 @@
 #' output <- eco_activities_gpt_validation(eco_activities_after_keywords_search)
 #' output
 eco_activities_gpt_validation <- function(data) {
-  # api_key <- readLines('/Users/kalashsinghal/git/2DII/tilt.eu.taxonomy/access_key.txt')
-  # api_key <- readLines('./inst/extdata/access_key.txt')
-
-  package_dir <- system.file(package = "tilt.eu.taxonomy")
-
-  # Specify the path to the .txt file within the package directory
-  file_path <- file.path(package_dir, "extdata", "access_key.txt")
-
-  # Read the content of the .txt file
-  api_key <- readLines(file_path)
+  # file_path <- file.path(getwd(), "inst", "extdata", "access_key.txt")
+  api_key <- readLines(system.file("access_key.txt", package = "tilt.eu.taxonomy"))
+  # api_key <- readLines('./access_key.txt')
 
   gpt_validation <- data |>
     mutate(gpt_validation = unlist(Map(gpt_validate, data$ecoinvent_activity_name, data$tax_activity_description)))
@@ -70,5 +63,6 @@ gpt_validate <- function(ecoinvent, taxonomy) {
       messages = list(list("role" = "user", "content" = prompt_with_data))
     )$choices$message.content
   }
-  output
+
+  return(output)
 }
